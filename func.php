49b6e3b6b23994d7bc0932209a889f56dc588879
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2019-03-30 10:10:42 +0700

    Update func.php
    Add even, odd and prime number identifier functiion

--- func.php
+++ func.php
@@ -24,16 +24,16 @@ function tahunKabisat()
                             if (mysqli_num_rows($sql_query) > 0) {
                                 if ($_GET['tahun'] % 4 == 0) {
                                     echo '<div class="alert alert-success text-center w-100 my-3" role="alert">
-                                                Tahun adalah tahun kabisat
+                                                Tahun ' . $_GET['tahun'] . ' adalah tahun kabisat
                                             </div>';
                                 } else {
                                     echo '<div class="alert alert-danger text-center w-100 my-3" role="alert">
-                                                Tahun bukan tahun kabisat
+                                                Tahun ' . $_GET['tahun'] . ' bukan tahun kabisat
                                             </div>';
                                 }
                             } else {
                                 echo '<div class="alert alert-danger text-center w-100 my-3" role="alert">
-                                                Tahun tidak ada di database
+                                                Tahun ' . $_GET['tahun'] . ' tidak ada di database
                                             </div>';
                             }
                         }
@@ -61,7 +61,7 @@ function hris()
     ?>
 <div class="container-fluid">
     <div class="row justify-content-md-center">
-        <div class="col col-sm-7 my-5 pt-5">
+        <div class="col col-xl-7 my-5 pt-5">
             <div class="card card-signin">
                 <div class="card-body">
                     <div class="alert alert-success text-center w-100" role="alert">
@@ -77,7 +77,7 @@ function hris()
                                     <th>Nama</th>
                                     <th>Departemen</th>
                                     <th>Level</th>
-                                    <th>Action</th>
+                                    <th colspan="5">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -86,8 +86,8 @@ function hris()
                                     $nomor = 1;
                                     while ($row = mysqli_fetch_assoc($sql_query)) {
                                         echo '<tr><td>' . $nomor . '</td><td>' . $row['nik'] . '</td><td>' . $row['nama'] . '</td><td>' . $row['nama_departemen'] . '</td><td>' . $row['level_karyawan'] . '</td>
-                                        <td><a href="update.php/?id=' . $row['id'] . '"><button type="button" class="btn btn-outline-primary">Update</button></a> &nbsp;
-                                        <a href="hapus.php?id=' . $row['id'] . '"><button type="button" class="btn btn-outline-danger">Delete</button></a></td></tr>';
+                                        <td><a href="update.php/?id=' . $row['id'] . '"><button type="button" class="btn btn-outline-primary mr-0">Update</button></a>
+                                        <a href="hapus.php?id=' . $row['id'] . '"><button type="button" class="btn btn-outline-danger ml-0">Delete</button></a></td></tr>';
                                         $nomor++;
                                     }
                                 }
@@ -106,4 +106,95 @@ function hris()
     <?php
 
 }
-?> 
+?>
+    <?php
+    function angka()
+    { ?>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col col-sm-7 my-5 pt-5">
+                <div class="card card-signin">
+                    <div class="card-body">
+                        <div class="alert alert-success text-center w-100" role="alert">
+                            Login Success!!
+                        </div>
+                        <h5 class="card-title text-center">Cek Bilangan</h5>
+
+                        <form class="form-group" method="get" action="">
+                            <!-- Example single danger button -->
+                            <select class="form-control form-control-md" name="pilihan">
+                                <option>Ganjil</option>
+                                <option>Genap</option>
+                                <option>Prima</option>
+                            </select>
+
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase my-3" type="submit">Tampilkan</button>
+                            <?php 
+                            if (isset($_GET['pilihan']) and $_GET['pilihan'] != '') {
+                                $conn  = mysqli_connect("127.0.0.1", "root", "", "uas");
+                                $sql = 'SELECT * from bilangan';
+                                $sql_query = mysqli_query($conn, $sql);
+                                switch ($_GET['pilihan']) {
+                                    case 'Ganjil':
+                                        echo '<div class="alert alert-info text-center w-100 my-1" role="alert">
+                                            Ini adalah bilangan ganjil 
+                                            </div>';
+                                        while ($row = mysqli_fetch_assoc($sql_query)) {
+                                            //echo '1';
+                                            if ($row['bilangan'] == 1) {
+                                                echo '1';
+                                            }
+                                            if ($row['bilangan'] % 2 != 0 && $row['bilangan'] != 1) {
+                                                echo ' ,' . $row['bilangan'];
+                                            }
+                                        }
+                                        break;
+                                    case 'Genap':
+                                        echo '<div class="alert alert-info text-center w-100 my-1" role="alert">
+                                             Ini adalah bilangan genap 
+                                            </div>';
+                                        while ($row = mysqli_fetch_assoc($sql_query)) {
+                                            //echo '1';
+                                            if ($row['bilangan'] == 2) {
+                                                echo '2';
+                                            }
+                                            if ($row['bilangan'] % 2 == 0 && $row['bilangan'] != 2) {
+                                                echo ' ,' . $row['bilangan'];
+                                            }
+                                        }
+                                        break;
+                                    case 'Prima':
+                                        echo '<div class="alert alert-info text-center w-100 my-1" role="alert">
+                                            Ini adalah bilangan prima 
+                                            </div>';
+                                        while ($row = mysqli_fetch_assoc($sql_query)) {
+                                            //echo '1';
+                                            if ($row['bilangan'] == 2) {
+                                                echo '2';
+                                            }
+                                            if ($row['bilangan'] > 2) {
+                                                $pembagi = 0;
+                                                for ($i = 1; $i <= $row['bilangan']; $i++) {
+                                                    if ($row['bilangan'] % $i == 0) {
+                                                        $pembagi++;
+                                                    }
+                                                }
+                                                if ($pembagi < 3) {
+                                                    echo ' ,' . $row['bilangan'];
+                                                }
+                                            }
+                                        }
+                                        break;
+                                }
+                            }
+                            ?>
+                            <hr class="my-4">
+                        </form>
+                        <div class="float-right"><a href="logoff.php"><button class="btn btn-dark btn-lg" type="submit">Logoff</button></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php 
+} ?> 
